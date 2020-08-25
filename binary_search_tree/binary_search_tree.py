c656f653bb10bf5443467dc1c4444dee548ba69c
Amber-Pittman <amber@amberpittman.com>
2020-08-25 19:38:25 -0400

    Part 1 passing tests

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,63 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # is the value less than the Node's value?
+        if value < self.value:
+            # if no left value set the left to the new node
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                # if left already taken, insert the left into a new node with a new value
+                self.left.insert(value)
+        #is the value more than the Node's value?
+        if value >= self.value:
+            #if no right value, set the right to the new node
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                #if right already taken, insert the right into a new node with a new value
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        #check if the value is the same as the target already
+        if self.value == target:
+            return True
+        #otherwise, declare that it has not been found yet
+        found = False
+        #compare target to the current value
+        if self.value >= target:
+            # is it in the left subtree?
+            if self.left is None:
+                return False
+            found = self.left.contains(target)
+        if self.value < target:
+            # is it in the right subtree?
+            if self.right is None:
+                return False
+            found = self.right.contains(target)
+        return found 
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # largest values always go to the right
+        if self.right is None:
+            # if we can't go right, return the current value
+            return self.value
+        # if we know we can go to the right, call get max on the right and return it
+        max = self.right.get_max()
+        return max 
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        if self.left:
+            self.left.for_each(fn)
+
+        fn(self.value)
+
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
@@ -73,13 +116,13 @@ bst.insert(3)
 bst.insert(4)
 bst.insert(2)
 
-bst.bft_print()
-bst.dft_print()
+# bst.bft_print()
+# bst.dft_print()
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# print("in order")
+# bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()  
